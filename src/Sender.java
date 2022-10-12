6ee482a7769b74f728ef03cd5b62bded7a4f7c8d
Adriana Miller <acmiller238@gmail.com>
2022-10-11 20:36:27 -0700

    Finishing Sender.java code

--- src/Sender.java
+++ src/Sender.java
@@ -31,16 +31,32 @@ public class Sender{
     // Buffer for hashing large files
     private static int BUFFER_SIZE = 32 * 1024;
     // IV for AES encryption
-    static String IV = "AAAAAAAAAAAAAAAA";
+    static String IV = "HJDSLBWERAYTIZQP";
+    private static PublicKey xPublicK;
+    private static PublicKey xPrivateK;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, BadPaddingException, InvalidKeySpecException,
+        NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, Exception {
+        
+        // Step 3: Read user input
+        System.out.print("Input the name of the message file: ");
+        String fileName = sysIn.next();
         try {
-            PublicKey xPublicK = readPubKeyFromFile("XPublic.key");
-            PrivateKey xPrivateK = readPrivKeyFromFile("XPrivate.key");
-            PublicKey yPublicK = readPubKeyFromFile("YPublic.key");
-            PrivateKey yPrivateK = readPrivKeyFromFile("YPrivate.key");
-            byte[] symKey = loadSymmetricKey();
+            // Step 1: Read information on keys 
+            System.out.println("Modulus and exponents from XPublic and XPrivate Keys");
+            readPubKeyFromFile("XPublic.key");
+            readPrivKeyFromFile("XPrivate.key");
+            System.out.println(loadSymmetricKey());
+
+            // Step 4: Calculate SHA256
+            byte[] hash = getSHA(fileName);
+            getSHA(fileName);
+            // Step 5: Calculate RSA encryption of SHA256
+            String RSAstring = RSAencrypt(hash);
+            RSAencrypt(hash);
+            // Step 6: Calculate 
+            encrypt(RSAstring);
             
         } catch (IOException e) {
             System.out.println("Error finding/reading keys");
@@ -94,27 +110,20 @@ public class Sender{
         return symKey;
     }
 
-    public static String getUserInput() throws FileNotFoundException {
-        // Step 3: Get user input
-        System.out.print("Input the name of the message file: ");
-        // Read user input
-        String fileName = sysIn.next();
-        return fileName;
+    private static void printHashFromString(byte[] hash){
+        System.out.println("digital digest (hash value):");
+        for (int k=0; k<hash.length; k++) {
+            System.out.format("%2X ", hash[k]) ;
+            if (k + 1 % 16 == 0) System.out.println("");
+        }
+        System.out.println("");
+
     }
 
-    /** Create method to check if input is valid
-    private static boolean isValid(String answer) {
-        if (answer == "Y" || answer == "N") {
-            return true;
-        }
-        else return false;
-    } */
-
     // Step 4: Calculate hash value
-    public static byte[] getSHA(String[] args) throws FileNotFoundException, NoSuchAlgorithmException,
+    public static byte[] getSHA(String msgFileName) throws FileNotFoundException, NoSuchAlgorithmException,
     IOException {
-        String fileName = getUserInput();
-        BufferedInputStream file = new BufferedInputStream(new FileInputStream(fileName));
+        BufferedInputStream file = new BufferedInputStream(new FileInputStream(msgFileName));
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         DigestInputStream in = new DigestInputStream(file, md);
 
@@ -128,6 +137,7 @@ public class Sender{
 
         byte[] hash = md.digest();
 
+        printHashFromString(hash);
         System.out.println("digit digest (hash value):");
         for (int k=0, j=0; k < hash.length; k++, j++) {
           System.out.format("%2X ", hash[k]) ;
@@ -137,31 +147,15 @@ public class Sender{
           }
           System.out.println("The SHA(M) in Hexidecimal bytes: " + hash);
 
-        Scanner invert = new Scanner(System.in);
         System.out.println("Do you want to invert the first byte in SHA256(M)? (Y or N)");
-        String answer = invert.nextLine();
-        
-        // If answer is valid continue
-        // if (isValid(answer)) 
+        String answer = sysIn.nextLine();
+        // TO DO: While !validIn
         if (answer == "Y"){
-            for (int index = 0; index < hash.length; index++){
-                // If answer is yes, replace firt byte with bitwise its inverted value
-                // hash[0] = hash[~0 & 0xff];
-                int x = hash[0];
-                x = ~x & 0xff;
-                hash[0] = hash[x];
+                // If answer is yes, replace firt byte with bitwise its inverted valu
+                hash[0] = (byte) ~hash[0];
                 System.out.println("You answered: " + answer + "The first bit is now: " + hash[0]);
             }
         }
-        if (answer == "N"){
-            // If answer is "N" no change
-            System.out.println("You answered: " + answer + ". There is no change to the first bit.");
-        }
-        else {
-            System.out.println("That was not a valid input, please try again.");
-            answer = invert.nextLine();
-        }
-        invert.close();
 
         // Save hash to file message.dd
         BufferedOutputStream shaMessageFile = new BufferedOutputStream(new FileOutputStream("message.dd"));
@@ -170,13 +164,14 @@ public class Sender{
         shaMessageFile.close();
         // Display hash
         System.out.println("The hash is: " + Arrays.toString(hash));
-        }
+
         return hash;
     }
     
     // Step 5: Calculate RSA Encryption
-    public static byte[] RSAencrypt(String data, String privateString,  byte[] hash) throws NoSuchAlgorithmException, InvalidKeySpecException,
+    public static String RSAencrypt(byte[] hash) throws NoSuchAlgorithmException, InvalidKeySpecException,
     BadPaddingException, NoSuchPaddingException, IOException, InvalidKeyException, IllegalBlockSizeException{
+        // byte [] hash = getSHA();
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         cipher.init(Cipher.ENCRYPT_MODE, readPrivKeyFromFile("XPrivate.key"));
         byte[] cipherText = cipher.doFinal(hash);
@@ -184,56 +179,59 @@ public class Sender{
         System.out.println("CipherText block size is " 
         +  cipherText.length + " bytes");
 
+        System.out.println("The hash is: " + Arrays.toString(hash));
         //Convert byte array to hex value
         for (int ind=0, j=0; ind < cipherText.length; ind++, j++) {
             System.out.format("%02X ", cipherText[ind]);
-            String cipherHash = String.format("%02x", cipherText[ind]);
             if (j >= 15) {
                 System.out.println("");
                 j=-1;
                 }
             }
             System.out.println("");
-        
+    
         BufferedOutputStream RSAencryption = new BufferedOutputStream(new FileOutputStream("message.ds-msg"));
-        // TO DO: How to pass cipherHash to file
-        //###########################################################################################################
-        // Save hex value of cipher text to message.ds-msg file
+
+        RSAencryption.write(hash, 0, hash.length);
         RSAencryption.write(cipherText, 0, cipherText.length);
         RSAencryption.close();
 
-        return cipherText; // Not needed?
+        return new String(cipherText, StandardCharsets.UTF_8);
     }
-    // Step 6: Calulate AES Encryption
-    public static byte[] encrypt() throws Exception {
-        
-        // Calulate AES Encryption using sym key KXY by reading file "message-ds.msg"
-        // (RSA-En Kx– (SHA256 (M)) || M) 
 
-        // Variable to read 16 bytes
-        int length = 16;
-        byte[] readBytes = new byte[length];
+    // Step 6: Calulate AES Encryption
+    public static byte[] encrypt(String RSAstring) throws Exception {
+
+        // String RSAstring = RSAencrypt();
+
         // Load Symmetric key
         byte[] symKey = loadSymmetricKey(); 
 
-        // WHEN/HOW TO USE SYMMETRIC KEY??
-
-        /** If the length of the last peice is less than that of 16 bytes, it needs to be 
-         * placed in a byte array whose array size is the length of the last piece is the
-         * length of the last piece before being encrypted */
-
-        FileInputStream message = new FileInputStream("message.ds-msg");
-        message.read("message.ds-msg");
+        FileInputStream file = new FileInputStream("message.ds-msg");
+        BufferedOutputStream AESencryption = new BufferedOutputStream(new FileOutputStream("message.aescipher"));
 
         Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
-        SecretKeySpec key = new SecretKeySpec(message.getBytes("UTF-8"), "AES");
-        cipher.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+        SecretKeySpec key = new SecretKeySpec(symKey, "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
 
-        // Save resulting blocks of AES ciphertext into a file named "message.aescipher"
-        BufferedOutputStream AESencryption = new BufferedOutputStream(new FileOutputStream("message.aescipher"));
-        AESencryption.write(cipherText, 0, cipherText.length);
+        // Read 16 bytes at a time and write to file message.aescipher
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int numBytesRead;
+        while (true) {
+            numBytesRead = file.read(buffer, 0, buffer.length);
+            if(numBytesRead <= 0) break;
+            if(numBytesRead == BUFFER_SIZE) {
+                AESencryption.write(cipher.doFinal(buffer, 0, numBytesRead));
+            } else {
+                byte[] smallBuffer = new byte[numBytesRead];
+                System.arraycopy(buffer, 0, smallBuffer, 0, numBytesRead);
+                AESencryption.write(cipher.doFinal(smallBuffer, 0, numBytesRead));
+            }
+        }
+        System.out.println("Final encrypted file: " + AESencryption);
+
         AESencryption.close();
-
-        return cipher.doFinal(message.getBytes("UTF-8"));
+        file.close();
+        return cipher.doFinal(RSAstring.getBytes("UTF-8"));
     }
 }
